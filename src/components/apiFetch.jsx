9183a1728ffda41fcc3ebf17c7284e2ec8bf3ef7
Pratham08 <prathampali0909@gmail.com>
2023-06-16 20:55:44 +0530

    h

---
+++ src/components/apiFetch.jsx
@@ -0,0 +1,14 @@
+
+const getData = async function (city) {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=e8f064c3b8a78085a12b329aec9faeb3&units=metric";
+    var data = await fetch(url)
+        .then((res) => res.json())
+        .then((data) => data);
+    return data;
+    // console.log(data);
+};
+
+export default getData;
